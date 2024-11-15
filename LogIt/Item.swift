8d90daf5dfd13dee683708ee45b1d2e0a612921f
Ettore Ferranti <ettore.ferranti@gmail.com>
2024-11-15 19:29:22 +0100

    Initial Commit

---
+++ LogIt/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  LogIt
+//
+//  Created by Ettore Ferranti on 15.11.2024.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ LogItTests/LogItTests.swift
@@ -0,0 +1,17 @@
+//
+//  LogItTests.swift
+//  LogItTests
+//
+//  Created by Ettore Ferranti on 15.11.2024.
+//
+
+import Testing
+@testable import LogIt
+
+struct LogItTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
